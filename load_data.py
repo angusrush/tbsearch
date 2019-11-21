13f5e51ed98e888ee04f2a0c0a62406c73c49a4e
Angus Rush <angus.h.rush@gmail.com>
2019-11-21 11:08:01 +0100

    Added auto-entry function

--- load_data.py
+++ load_data.py
@@ -1,9 +1,19 @@
 import sqlite3
+import os
 
 def enter_tb(title, author, location):
     c.execute("INSERT INTO textbooks (title, author, location) VALUES (?, ?, ?)",
             (title, author, location))
     conn.commit()
+
+def auto_enter(title, author, location):
+    filename = author.split(' ')[-1] + '_' + title.replace(' ', '_') + '.pdf'
+    newpath = '/home/angus/Textbooks/math/' + filename
+    #print("Path: " + newpath)
+    os.rename(os.path.abspath(location), newpath)
+    enter_tb(title, author, newpath)
+    
+
 
 # This doesn't work and I'm not sure why.
 def rename_author(oldname, newname):
@@ -40,6 +50,7 @@ def enter_data():
 conn = sqlite3.connect('mathdoc.db')
 c = conn.cursor()
 
+#auto_enter("A Short Course on Infinity-categories", "Moritz Groth", "/home/angus/Short.pdf")
 
 c.close()
 conn.close()
